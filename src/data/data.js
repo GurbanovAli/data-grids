19dd6f4484c226ed1a998eb451a3e911533b1c4b
GurbanovAli <aligurbanov95@mail.ru>
2020-03-05 23:09:56 +0300

    add: all files

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+
+export const data = [
+   {
+      id:1,
+      country: 'USA',
+      population: 333,
+      economy:6776,
+      averageSalary: 'high'
+   },{
+      id:2,
+      country: 'USA',
+      population: 333,
+      economy:6776,
+      averageSalary: 'high'
+   },{
+      id:3,
+      country: 'USA',
+      population: 333,
+      economy:6776,
+      averageSalary: 'high'
+   },{
+      id:4,
+      country: 'USA',
+      population: 333,
+      economy:6776,
+      averageSalary: 'high'
+   },{
+      id:5,
+      country: 'USA',
+      population: 333,
+      economy:6776,
+      averageSalary: 'high'
+   }
+]
