7ddcf1592d7159ae00922e5686a0f1aa5e0852e2
GurbanovAli <aligurbanov95@mail.ru>
2020-03-17 23:24:18 +0300

    change div

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 
 import "./index.css";
-
 import Loader from "./Table";
 
 class App extends Component {
   render() {
     return (
-      <>
+      <div>
         <Loader />
-      </>
+      </div>
     );
   }
 }
